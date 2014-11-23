cc1d9f08e8ee7d18659bc21ff73f31274244ba5b
AlexandraVillepique <aleksandra.andic@gmail.com>
2014-11-23 11:35:17 -0800

    Simple Solution
    A limited solution for the Programming Assignment 2. The better option
    is to use R.cache package.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+############################################################################
+# cachematrix.R
+#
+#"Generates a cache of solve operation in different enviroment"
+#
+# A simple code that makes a list which stores matrix and inverse of the matrix.
+# It is assumed that input matrix is squared and inversiable. Code does not ch-
+# ecks validity of input matrix. 
+# For more flexible solution look up package "R.cache". 
+#
+#
+# Author: Aleksandra Villepique
+# Date: 2014-11-23
+#
+##########################################################################
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+      ## makes list that cashes matrix and inverse matrix
+      xi<=NULL
+      set <- function(y){
+            x <<- y
+            xi <<- NULL
+      }
+      get <- function() x
+      setinverse < - function(inverse) xi << - solve
+      getinverse <- function() xi
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Following function calculates the inverse of the matrix that is part of 
+## construct created with makeCacheMatrix. If the inverse is already calculated
+## it skips calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      xi <-x$getinverse()
+      if(!is.num(xi)){
+            message("getting cahsed data")
+            return(xi)
+      }
+      data <-x$get()
+      datai <-solve(data,...)
+      x$setinverse(datai)
+      datai
 }
